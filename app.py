1bf4aa567a14fbccb91c871f1ff03346c1a6213c
fay <wahomeimani@gmail.com>
2024-01-18 10:52:13 +0300

    completed requests

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_restful import Api, Resource
+import os
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 api = Api(app)
@@ -8,9 +10,22 @@ api = Api(app)
 class GoodMorning(Resource):
     def get(self):
         return {"message": "Good morning! How are you today?"}
-
+#handling post request
+    def post(self):
+        return {"message":f"Good Morning !  Data recieved:"}
+#handling patch request
+    def patch(self,name=""):
+        return {"message": f"Hello {name}, how are you doing? Patch Request!"}
+#handling a put request
+    def put(self):
+        return {"message":"I am putting data"}
+       
 # Adding the URL route and the method to be called when this route is accessed
 api.add_resource(GoodMorning, '/')
+
+
+
+
 
 
 if __name__ == '__main__':
