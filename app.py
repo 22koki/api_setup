05081e78da678b6f8efcd100b4e1351d89647061
fay <wahomeimani@gmail.com>
2024-01-18 11:40:02 +0300

    task app

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Api, Resource
 import os
 from flask import Flask, jsonify
@@ -25,15 +25,61 @@ api = Api(app)
 
 
 
-class UserResource(Resource):
-    def get(self, user_id=None):
-        if user_id:
-            return {"user_id": user_id, "username": "name"}
-        else:
-            return {"message": "Welcome new user!"}
 
-# Modified route definition to include an optional user_id parameter
-api.add_resource(UserResource, '/', '/user/<int:user_id>')
+#class UserResource(Resource):
+ #   def get(self, user_id=None):
+  #      if user_id:
+   #         return {"user_id": user_id, "username": "name"}
+    #    else:
+     #       return {"message": "Welcome to the root URL!"}
+#
+## Modified route definition to include an optional user_id parameter
+#api.add_resource(UserResource, '/', '/user/<int:user_id>')
+
+
+#FLASKRESTFUL FOR HANDLING TASK OPERATIONS LIKE CREATING UPDATING AND MARKING TASKS AS COMPLETED
+class Tasks(Resource):
+    tasks = {}
+    task_id_counter = 1
+
+    def get(self, task_id=None):
+        if task_id:
+            task = self.tasks.get(task_id)
+            if task:
+                return {task_id: task}
+            else:
+                return {"message": f"Task with ID {task_id} not found"}
+        else:
+            return {'tasks': self.tasks}
+
+    def post(self):
+        task_id = Tasks.task_id_counter
+        title = request.json.get('title', '')
+        task = {'id': task_id, 'title': title}
+        Tasks.tasks[task_id] = task
+        Tasks.task_id_counter += 1
+        return task
+
+    def put(self, task_id):
+        task = Tasks.tasks.get(task_id)
+        if not task:
+            return {'message': f'Task with ID {task_id} not found'}
+        else:
+            for key, value in request.json.items():
+                task[key] = value
+            return task
+
+    def delete(self, task_id):
+        if task_id in Tasks.tasks:
+            del Tasks.tasks[task_id]
+            return {'message': f'Task with ID {task_id} has been deleted'}
+        else:
+            return {'message': f'Task with ID {task_id} not found'}
+
+
+api.add_resource(Tasks, '/', '/tasks/<int:task_id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+       
